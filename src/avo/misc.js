ee76ef51e12d95abc70b1825831a1fbf33dce93a
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2021-08-21 20:48:15 +0100

    Add angleDiff() utility function

--- src/avo/misc.js
+++ src/avo/misc.js
@@ -7,3 +7,16 @@ a mathematical 0 to be represented as something incredibly small like
 export function isZero (num) {
   return -1e-10 < num && num < 1e-10
 }
+
+/*
+Finds the difference between angle B and angle A, in radians.
+ */
+export function angleDiff (angleA, angleB) {
+  let diff = angleB - angleA
+  
+  // Clamp diff value to -180º <= x <= +180º
+  while (diff < -Math.PI) diff += Math.PI * 2  // While diff < -180º, rotate by +360º
+  while (diff > Math.PI) diff -= Math.PI * 2  // While diff > 180º, rotate by -360º
+    
+  return diff
+}
