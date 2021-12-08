928ee0a9e6393f0a003b9e792509096985c5da3f
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2021-12-08 20:26:43 +0800

    Fix typo

--- src/avo/routine/README.js
+++ src/avo/routine/README.js
@@ -6,4 +6,4 @@ For example, if the player reaches the exit of a dungeon, we can start the
 "VictoryCountdownRoutine" to transition the game from a playable state to the
 "Congratulations, you won!" screen, over 5 seconds, with some visual flourish.
 
-- `Routine.js` provides the base code for all Transition types.
+- `Routine.js` provides the base code for all Routine types.
