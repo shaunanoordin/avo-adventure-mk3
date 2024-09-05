e892e20e75d74bb588f42a31ea731f4a0d78c9a0
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2024-09-05 03:28:10 +0100

    Tile: add paintSprite() convenience function

--- src/starter-story/tiles/floor-tile.js
+++ src/starter-story/tiles/floor-tile.js
@@ -13,30 +13,10 @@ export default class FloorTile extends Tile {
   }
 
   paint (layer = 0) {
-    const app = this._app
-    const c2d = app.canvas2d
-    if (!this.spriteSheet) return
-
-    app.applyCameraTransforms()
-
     if (layer === LAYERS.BACKGROUND) {
-      const srcX = 0 * this.spriteSizeX
-      const srcY = 2 * this.spriteSizeY
-      const sizeX = this.spriteSizeX
-      const sizeY = this.spriteSizeY
-      
-      c2d.translate(this.x, this.y)
-      c2d.scale(this.spriteScale, this.spriteScale)
-
-      const tgtX = this.spriteOffsetX
-      const tgtY = this.spriteOffsetY
-
-      c2d.drawImage(this.spriteSheet.img,
-        srcX, srcY, sizeX, sizeY,
-        tgtX, tgtY, sizeX, sizeY
-      )
+      this.paintSprite()
     }
-
-    app.undoCameraTransforms()
   }
+
+  getSpriteRow () { return 2 }
 }
